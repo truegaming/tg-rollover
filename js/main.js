de381904ea36f7beaa034207a56ebf3d76f30119
Jaber Zarif <jaber.zarif@outlook.fr>
2022-09-21 08:29:46 +0100

    fix services cards

--- js/main.js
+++ js/main.js
@@ -143,27 +143,63 @@ function replace() {
 
 
 
-// Get the button
-let mybutton = document.getElementById("top");
+//Get the button 
+btnTop = document.getElementById("btnTop");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function () {
-    scrollFunction()
+  scrollFunction()
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  jQuery("html, body").animate({
+    scrollTop: "0"
+  }, 1500);
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+$("a").click(function() {
+	$('html,body').animate({
+		scrollTop: $("#about").offset().top
+	}, 1500);
+
+    $('html,body').animate({
+		scrollTop: $("#services").offset().top
+	}, 1500);
+
+    $('html,body').animate({
+		scrollTop: $("#projects").offset().top
+	}, 1500);
+    
+    $('html,body').animate({
+		scrollTop: $("#social").offset().top
+	}, 1500);
+    
+    $('html,body').animate({
+		scrollTop: $("#clients").offset().top
+	}, 1500);
+
+    $('html,body').animate({
+		scrollTop: $("#teams").offset().top
+	}, 1500);
+
+    $('html,body').animate({
+		scrollTop: $("#contact").offset().top
+	}, 1500);
+});
+
+
 
 /**
  * Show menu mobile
@@ -207,5 +243,29 @@ function HideMenu() {
         document.getElementById("menuMobile").style.display = "none";
     }
 }
+
+// $("#top").click(function() {
+//     $('html,body').animate({
+//       scrollTop: $("body").offset().top
+//     }, 3500);
+// });
+  
+//   $("#scrollTwo").click(function(e) {
+//     $('html,body').animate({
+//       scrollTop: $("#two").offset().top
+//     }, 750);
+//   });
+  
+//   $("#scrollThree").click(function() {
+//     $('html,body').animate({
+//       scrollTop: $("#three").offset().top
+//     }, 750);
+//   });
+  
+//   $("#scrollFour").click(function() {
+//     $('html,body').animate({
+//       scrollTop: $("#four").offset().top
+//     }, 750);
+//   });
 
 
